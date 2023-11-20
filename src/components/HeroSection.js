7dcb3b244536c5df47cdb76c03073ea3dcc2358e
conorkeane <g00391846@atu.ie>
2023-11-20 16:30:29 +0000

    Added HeroSection js file and code

---
+++ src/components/HeroSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "./Button";
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className="HeroSection-container">
+      <video src="/videos/video (720p).mp4" autoPlay loop muted />
+      <h1>Good Evening</h1>
+      <p>How are you today</p>
+      <div className="HeroSection-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Get Started
+        </Button>
+
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Testing <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
